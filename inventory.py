50ff3ed73c5d12676aceee06d7d3a19a39514124
KyanaConway <89709762+KyanaConway@users.noreply.github.com>
2022-05-03 09:59:22 -0500

    Create inventory.py

---
+++ inventory.py
@@ -0,0 +1,23 @@
+class Inventory:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.books = [] ##empty list for books
+
+class Books:
+    def __init__(self, name, id, quantity, price ):
+        self.name = name
+        self.id = id
+        self.quantity = quantity
+        self.price = price
+
+##formula showing complete price of same book
+    @property
+    def combinedPrice(self):
+        return f'${self.quantity * self.price:.2f}'
+
+##class to show the books id, price, and name
+    def show(self):
+        id = 1
+        for book in self.books:
+            print(str(f'{id} -> [x{book.price}] {book.name}'))
+            id += 1
